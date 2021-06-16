a64e57df3c077f0b571725a2cd4cc463e2ae2019
Michael McLarnon <mmclar@gmail.com>
2021-06-16 11:39:24 -0400

    initial websocket server

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import './index.less';
 
 $(document).ready(() => {
     console.log('ready to go!');
+    const ws = new WebSocket('ws://localhost:9001');
+    ws.onopen = () => {
+        ws.send('ready');
+    };
 });
