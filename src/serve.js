8f897637742f8ac50713d54b3f31e2db36b150c9
Michael McLarnon <mmclar@gmail.com>
2021-06-16 12:46:30 -0400

    game loop

--- src/serve.js
+++ src/serve.js
@@ -1,12 +1,40 @@
 const WebSocket = require('ws');
 const server = new WebSocket.Server({
-  port: 9001
+  port: 9001,
 });
+
+const gameState = {
+    players: [],
+}
 
 server.on('connection', function(socket) {
     console.log('connected');
 
+    gameState.players['a'] = {
+        name: 'b',
+    }
+
     socket.on('message', function(msg) {
         console.log('got message ' + msg);
     });
+
+    startGame();
 });
+
+const startGame = () => {
+    gameState.ball = {
+        position: [0, 0],
+    }
+
+    gameLoop();
+}
+
+const gameLoop = () => {
+    console.log('game loop');
+    updateBallPosition(gameState);
+    setTimeout(gameLoop, 100);
+}
+
+const updateBallPosition = () => {
+
+};
