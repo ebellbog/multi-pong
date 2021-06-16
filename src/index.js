c7a2b84ef8d8538be49636f1cd71e1b5a3168c29
Elana Bell Bogdan <e.bell.bogdan@gmail.com>
2021-06-16 12:55:23 -0400

    setup paddles; listen for arrow key presses

--- src/index.js
+++ src/index.js
@@ -5,8 +5,25 @@ const $game = $('#game');
 const gameSize = 500;
 const padding = 5;
 
+const $paddles = [];
+let paddleLength;
+
 $(document).ready(() => {
-    setupGame(8);
+    setupWalls(8);
+    setupPaddles(8);
+
+    $(document).on('keydown', ({which}) => {
+        switch(which) {
+            case 37:
+                console.log('left arrow');
+                break;
+            case 39:
+                console.log('right arrow');
+                break;
+            default:
+                break;
+        }
+    })
 
     // Test websockets
     const ws = new WebSocket('ws://localhost:9001');
@@ -15,17 +32,33 @@ $(document).ready(() => {
     };
 });
 
-function setupGame(numPlayers) {
+function setupWalls(numPlayers) {
     $game.attr('viewBox', `0 0 ${gameSize} ${gameSize}`);
     const radius = gameSize / 2 - padding;
     const centerPt = {x: gameSize / 2, y: gameSize / 2};
     const angleDelta = (Math.PI * 2) / numPlayers;
     for (let i = 0; i < numPlayers; i++) {
-        const startPt = projectPoint(centerPt, angleDelta * i, radius);
-        const endPt = projectPoint(centerPt, angleDelta * (i + 1), radius); 
-        drawLine(startPt, endPt).attr('stroke', `hsl(${360 * i / numPlayers} , 100%, 50%)`);
+        const startPt = projectAngle(centerPt, radius, angleDelta * i);
+        const endPt = projectAngle(centerPt, radius, angleDelta * (i + 1));
+        drawLine(startPt, endPt).addClass('wall').attr('stroke', `hsl(${360 * i / numPlayers} , 100%, 50%)`);
     }
 }
+
+function setupPaddles() {
+    $('line').each((idx, line) => {
+        const $line = $(line);
+        const center = getCenter($line);
+        const slope = getSlope($line);
+        if (!paddleLength) {
+            paddleLength = getLength($line) * 0.3;
+        }
+        const startPt = projectSlope(center, -paddleLength/2, slope);
+        const endPt = projectSlope(center, paddleLength/2, slope);
+        const $paddle = drawLine(startPt, endPt).addClass('paddle').attr('stroke', $line.attr('stroke'));
+        $paddles.push($paddle);
+    });
+}
+
 
 // SVG utility methods
 
@@ -41,11 +74,38 @@ function drawLine({x: x1, y: y1}, {x: x2, y: y2}) {
 
 // Math utility methods
 
-function projectPoint(start, angle, dist) {
+function projectAngle(start, dist, angle) {
     return {
         x: start.x + Math.cos(angle) * dist,
         y: start.y + Math.sin(angle) * dist
     }
+}
+
+function projectSlope(start, dist, slope) {
+    const deltaX = Math.sqrt(Math.pow(dist, 2) / (Math.pow(slope, 2) + 1)) * (dist > 0 ? 1 : -1);
+    return {
+        x: start.x + deltaX,
+        y: start.y + deltaX * slope
+    }
+}
+
+function getCenter($line) {
+    return {
+        x: (parseInt($line.attr('x2')) + parseInt($line.attr('x1'))) / 2,
+        y: (parseInt($line.attr('y2')) + parseInt($line.attr('y1'))) / 2
+    };
+}
+
+function getSlope($line) {
+    const deltaX = $line.attr('x2') - $line.attr('x1');
+    return (deltaX !== 0) ? ($line.attr('y2') - $line.attr('y1')) / deltaX : false;
+}
+
+function getLength($line) {
+    return Math.sqrt(
+        Math.pow($line.attr('x2') - $line.attr('x1'), 2) +
+        Math.pow($line.attr('y2') - $line.attr('y1'), 2)
+    );
 }
 
 function randFloat(min, max) {
