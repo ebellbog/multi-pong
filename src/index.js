d3bc71069e32e3036555ff4c5ab6213497d802b1
Tim Birkmire <tim.birkmire@hospiq.com>
2021-06-16 14:05:04 -0400

    Add new game button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import _ from 'lodash';
 import './index.less';
 
+const ws = new WebSocket('ws://localhost:9001');
+
 $(document).ready(() => {
     console.log('ready to go!');
-    const ws = new WebSocket('ws://localhost:9001');
+
     ws.onopen = () => {
         ws.send('ready');
     };
+
+    ws.onmessage = function (msg) {
+        console.log(msg.data);
+    };
 });
+
+$('#btn-new-game').on('click', () => {
+    ws.send('A new game has begun!')
+})
