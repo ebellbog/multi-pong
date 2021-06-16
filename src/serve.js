e8235f73d09043d3e19086c63343990896444874
Michael McLarnon <mmclar@gmail.com>
2021-06-16 12:37:57 -0400

    remove boilerplate

--- src/serve.js
+++ src/serve.js
@@ -3,18 +3,10 @@ const server = new WebSocket.Server({
   port: 9001
 });
 
-let sockets = [];
 server.on('connection', function(socket) {
     console.log('connected');
-    sockets.push(socket);
 
-    // When you receive a message, send that message to every socket.
     socket.on('message', function(msg) {
         console.log('got message ' + msg);
-    });
-
-    // When a socket closes, or disconnects, remove it from the array.
-    socket.on('close', function() {
-        sockets = sockets.filter(s => s !== socket);
     });
 });
